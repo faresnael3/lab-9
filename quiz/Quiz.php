3110ccf963dc80a9c8b5b8fe49ddb39780eb7978
Fares Elattar <fareselattar@ku.edu>
2020-11-16 17:21:37 -0600

    added ability to see your answer in th quiz

--- quiz/Quiz.php
+++ quiz/Quiz.php
@@ -25,5 +25,35 @@ if($five== "Abu-dhabi")
 {
   $x=$x+20;
 }
-echo "Your grade is: $x%";
+echo "Your grade is: $x%
+<br><br>
+Who is 448 professor?
+<br>
+You answered: $first
+<br>
+Correct answer: johnson
+<br><br>
+How many pyramids are there?
+<br>
+You answered: $second
+<br>
+Correct answer: three
+<br><br>
+Who won the US presdential election 2020?
+<br>
+You answered: $third
+<br>
+Correct answer: biden
+<br><br>
+What is my age?
+<br>
+You answered: $four
+<br>
+Correct answer: twenty
+<br><br>
+What is the capital of UAE?
+<br>
+You answered: $five
+<br>
+Correct answer: Abu-dhabi";
 ?>
